14776a0e2fb3102e3691b0447d47997cc3c510ad
unknown <vicker1>
2019-10-16 11:29:11 -0700

    jwtpackages

--- app/User.php
+++ app/User.php
@@ -4,9 +4,11 @@ namespace App\Model;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Model\Question;
+use App\Model\User;
 
-
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -32,4 +34,26 @@ class User extends Authenticatable
     {
         return $this->hasmany(Question::class);
     }
+
+    
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,18 @@ Route::post('/like/{reply}','Likecontroller@likeit');
 Route::delete('/like/{reply}','Likecontroller@unlikeit');
 
 
-//Route::group([
+Route::group([
 
-    //'middleware' => 'api',
-    //'prefix' => 'auth'  
+    'middleware' => 'api',
+    'prefix' => 'auth' 
+ ],
+    
+    function ($router) {
 
-//], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
 
-    //Route::post('login', 'AuthController@login');
-    //Route::post('signup', 'AuthController@signup');
-    //Route::post('logout', 'AuthController@logout');
-    //Route::post('refresh', 'AuthController@refresh');
-    //Route::post('me', 'AuthController@me');
-
-//});
+});
